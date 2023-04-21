a8a6006378d2b45b50c020170b2500f4e4c2ce1d
2121Gix <giacomocasagranda@proton.me>
2023-04-21 19:42:55 +0200

    Need 2 Install JQuery

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useState , useEffect, useMemo} from 'react';
-import { GoogleMap, useLoadScript } from '@react-google-maps/api';
+import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
+import axios from 'axios';
 
 function App() {
 
@@ -8,14 +9,32 @@ function App() {
   const [cs, setCs] = useState(0);
   const [round, setRound] = useState(1);
   const [city, setCity] = useState("");
+  const [mark, setMark] = useState({});
 
   const {isLoaded} = useLoadScript({
     googleMapsApiKey: 'AIzaSyCVeusViA1g24A4tSRPde0fEj9wq9kkebs'
   });
 
   useEffect(() => {
-    setCity("Atlanta");
+    fetchCity();
   }, [])
+
+  const fetchCity = () => {
+    
+    ajax({
+      method: 'GET',
+      url: 'https://api.api-ninjas.com/v1/city?min_population=100000',
+      headers: { 'X-Api-Key': process.env.NEXT_PUBLIC_NINJA_API_KEY},
+      contentType: 'application/json',
+      success: function(result) {
+          console.log(result);
+      },
+      error: (err) => {
+          setCity("Atlanta");
+      }
+  });
+
+  }
 
   const choiceConfirmed = () => {
     setCity("sium");
@@ -24,7 +43,12 @@ function App() {
 
   const replay = () => {};
 
-  const createMarker = () => {};
+  const placeMarker = (ev) => {
+    setMark({
+      lat: ev.latLng.lat(),
+      lng: ev.latLng.lng(), 
+    })
+  };
 
   return (
     <div className="App">
@@ -36,7 +60,17 @@ function App() {
       <div className='game'>
         <h4>(Round {round}/5) Point to: {city}</h4>
         <div className='map'>
-          <GoogleMap zoom={1} center={{lat: 0 , lng: 0}} mapContainerClassName='map'></GoogleMap>
+          <GoogleMap zoom={1} center={{lat: 0 , lng: 0}} mapContainerClassName='map' onClick={(ev) => placeMarker(ev)}>
+
+            {
+              mark !== null &&
+                <Marker
+              title= ""
+              name= ""
+              position={mark}
+            />
+            }           
+          </GoogleMap>
         </div>
         <button className='confirm' onClick={choiceConfirmed}>CONFIRM</button>
         <p>Current Score: {cs}</p>
